045a48b94ba3305e54b0d2493e857740412e60c2
Benny <benfroelich@gmail.com>
2016-09-24 00:30:08 -0700

    Initial Commit

---
+++ Cribbage on Mac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cribbage on Mac
+//
+//  Created by Ben Froelich on 9/24/16.
+//  Copyright © 2016 Ben Froelich. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
